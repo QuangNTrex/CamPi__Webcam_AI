65340ab4f9baa231109987ad3504259dec2c658f
quangntrex <quangnt.rex@gmail.com>
2026-03-24 17:27:01 +0700

    update test main

--- backend/main.py
+++ backend/main.py
@@ -16,8 +16,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-def stream_mjpeg():
+def mjpeg_stream():
     cmd = [
         "ffmpeg",
         "-f", "v4l2",
@@ -25,31 +24,30 @@ def stream_mjpeg():
         "-video_size", "640x480",
         "-framerate", "15",
         "-i", "/dev/video0",
-        "-c", "copy",
-        "-f", "mjpeg",
+        "-f", "image2pipe",
+        "-vcodec", "mjpeg",
         "-"
     ]
 
-    process = subprocess.Popen(
-        cmd,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.DEVNULL,
-        bufsize=0
-    )
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, bufsize=0)
 
     while True:
-        data = process.stdout.read(4096)
+        # đọc 1 frame JPEG đầy đủ
+        data = process.stdout.read(1024)
         if not data:
             break
-        yield data
-
+        yield (
+            b"--frame\r\n"
+            b"Content-Type: image/jpeg\r\n\r\n" + data + b"\r\n"
+        )
 
 @app.get("/video")
 def video_feed():
     return StreamingResponse(
-        stream_mjpeg(),
+        mjpeg_stream(),
         media_type="multipart/x-mixed-replace; boundary=frame"
     )
+
 
 # include router
 app.include_router(servo.router)
